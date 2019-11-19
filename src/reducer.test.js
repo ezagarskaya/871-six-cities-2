04453bc2af54f30286690b8ed1be9bc72aee61e6
ezagarskaya <e.zagarskaya@gmail.com>
2019-11-19 16:00:48 +0300

    fix tests for reducer

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -8,7 +8,7 @@ import cities from './mocks/cities.js';
 
 describe(`Action creators work correctly`, () => {
   it(`Action creator for changing city returns correct action`, () => {
-    expect(ActionCreator.changeCity()).toEqual({
+    expect(ActionCreator.changeCity(`Amsterdam`)).toEqual({
       type: ActionType.CHANGE_CITY,
       payload: cities[0].name,
     });
@@ -17,24 +17,29 @@ describe(`Action creators work correctly`, () => {
 
 describe(`Reducer works correctly`, () => {
   it(`Reducer without additional parameters should return initial state`, () => {
-    expect(reducer(undefined, {})).toEqual({
-      currentCity: cities[0].name,
+    const init = {
+      currentCity: cities[0],
       currentOffers: offers.filter((offer) => offer.city === cities[0].name),
       offers,
       cities,
-    });
+    };
+    expect(reducer(init, {})).toEqual(init);
   });
 
   it(`Reducer should change city`, () => {
     expect(reducer({
-      currentCity: cities[0].name,
+      currentCity: cities[0],
       currentOffers: offers.filter((offer) => offer.city === cities[0].name),
+      offers,
+      cities,
     }, {
       type: ActionType.CHANGE_CITY,
       payload: cities[1].name,
     })).toEqual({
-      currentCity: cities[1].name,
+      currentCity: cities[1],
       currentOffers: offers.filter((offer) => offer.city === cities[1].name),
+      offers,
+      cities,
     });
   });
 });
